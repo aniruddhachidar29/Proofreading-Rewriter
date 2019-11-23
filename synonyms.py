b637562a040017d2a4d3d259c51e7883085872f3
aniruddhachidar29 <aniruddha.chidar29@gmail.com>
2019-11-23 16:28:55 +0530

    synonyms-2

--- synonyms.py
+++ synonyms.py
@@ -5,12 +5,17 @@ from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
 import numpy
+import json
+import string
+# from pattern3.en import conjugate, lemma
+import urllib.request
+import re
 
 word = "run"
 synonym = "enemy"
 
-iitb_lingo_words=['machau','craxx','infi','scope','lingo','ditch','pain','tum-tum','lukkha','vela','enthu','huga','mugging','farra','ghati','junta','freshie','sophie']
-iitb_lingo_meanings=['rocking','cracked','infinite','scopeless','language','ditch','problem','bus','free','free','enthusiasm','blundered','studying','FR','local_resident','public','freshmen','sophomore']
+iitb_lingo_words=['machau','craxx','infi','scope','lingo','ditch','pain','tum-tum','lukkha','enthu','huga','mugging','farra','ghati','junta','freshie','sophie']
+iitb_lingo_meanings=['rocking','cracked','infinite','scopeless','language','ditch','problem','bus','free','enthusiasm','blundered','studying','FR','local_resident','public','freshmen','sophomore']
 iitb_lingo_dictionary={}
 for i in range(len(iitb_lingo_words)):
     iitb_lingo_dictionary[iitb_lingo_words[i]]=iitb_lingo_meanings[i]
@@ -34,30 +39,48 @@ def syn(word):
 
     pass
 
+def i_s(p):
+    sentences = re.split('[.,?!]', p);
+    # print(sentences)
+    n = 0
+    iss = []
+    for sentence in sentences:
+        sentence = sentence.lstrip()
+        words = sentence.split(' ')
+        isa = []
+        for word in words:
+            isa.append((word, n))
+            n += 1
+        # print(indexed_sentence)
+        iss.append(isa)
+    return iss
+    pass
+
+
 # define training dat
-sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-			['this', 'is', 'the', 'second', 'sentence'],
-			['yet', 'another', 'sentence'],
-			['one', 'more', 'sentence'],
-			['and', 'the', 'final', 'sentence']]
-# train model
-model = Word2Vec(sentences, min_count=1)
-# fit a 2d PCA model to the vectors
-X = model[model.wv.vocab]
-pca = PCA(n_components=2)
-result = pca.fit_transform(X)
-
-
-pyplot.scatter(result[:, 0], result[:, 1])
-words = list(model.wv.vocab)
-dic={}
-for i, word in enumerate(words):
-    dic[word]=i
-    pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
-pyplot.show()
-
-def similar(a,b):
-    numpy.linalg.norm(result[dic[i],2],resut[dic[i],1])
+# sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+# 			['this', 'is', 'the', 'second', 'sentence'],
+# 			['yet', 'another', 'sentence'],
+# 			['one', 'more', 'sentence'],
+# 			['and', 'the', 'final', 'sentence']]
+# # train model
+# model = Word2Vec(sentences, min_count=1)
+# # fit a 2d PCA model to the vectors
+# X = model[model.wv.vocab]
+# pca = PCA(n_components=2)
+# result = pca.fit_transform(X)
+#
+#
+# pyplot.scatter(result[:, 0], result[:, 1])
+# words = list(model.wv.vocab)
+# dic={}
+# for i, word in enumerate(words):
+#     dic[word]=i
+#     pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+# pyplot.show()
+#
+# def similar(a,b):
+#     numpy.linalg.norm(result[dic[i],2],resut[dic[i],1])
 
 
 # while(True):
