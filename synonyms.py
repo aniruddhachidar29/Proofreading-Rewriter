4e506d70019fa52d51242d6b5d156f3b2ef193fe
aniruddhachidar29 <aniruddha.chidar29@gmail.com>
2019-11-23 15:43:23 +0530

    synonyms

--- synonyms.py
+++ synonyms.py
@@ -1,10 +1,19 @@
 import nltk
 from nltk.corpus import wordnet
-
-
+from nltk.tokenize import sent_tokenize, word_tokenize
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+import numpy
 
 word = "run"
 synonym = "enemy"
+
+iitb_lingo_words=['machau','craxx','infi','scope','lingo','ditch','pain','tum-tum','lukkha','vela','enthu','huga','mugging','farra','ghati','junta','freshie','sophie']
+iitb_lingo_meanings=['rocking','cracked','infinite','scopeless','language','ditch','problem','bus','free','free','enthusiasm','blundered','studying','FR','local_resident','public','freshmen','sophomore']
+iitb_lingo_dictionary={}
+for i in range(len(iitb_lingo_words)):
+    iitb_lingo_dictionary[iitb_lingo_words[i]]=iitb_lingo_meanings[i]
 
 def similarity(a,b):
     wordFromList1 = wordnet.synsets(a)
@@ -25,4 +34,32 @@ def syn(word):
 
     pass
 
-print(set(syn(word)))
+# define training dat
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+			['this', 'is', 'the', 'second', 'sentence'],
+			['yet', 'another', 'sentence'],
+			['one', 'more', 'sentence'],
+			['and', 'the', 'final', 'sentence']]
+# train model
+model = Word2Vec(sentences, min_count=1)
+# fit a 2d PCA model to the vectors
+X = model[model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+
+
+pyplot.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.vocab)
+dic={}
+for i, word in enumerate(words):
+    dic[word]=i
+    pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
+
+def similar(a,b):
+    numpy.linalg.norm(result[dic[i],2],resut[dic[i],1])
+
+
+# while(True):
+#     a=input()
+#     breaked = word_tokenize(a)
