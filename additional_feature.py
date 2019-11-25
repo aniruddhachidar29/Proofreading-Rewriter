3b5b3c2264abcdfc54f8a956981af81833919dc4
Harshad <harshad.ingole99@gmail.com>
2019-11-25 12:08:59 +0530

    perfected punctuation

--- additional_feature.py
+++ additional_feature.py
@@ -1,5 +1,12 @@
 import nltk
-# nltk.download('nps_chat')
+#nltk.download('nps_chat')
+from nltk.corpus import brown
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
+brown_tagged_sents=brown.tagged_sents()
+brown_sents=brown.sents()
+ut=nltk.UnigramTagger(brown_tagged_sents)
+
 posts = nltk.corpus.nps_chat.xml_posts()[:10000]
 posts2 = nltk.corpus.nps_chat.xml_posts()[:]
 import re
@@ -24,10 +31,36 @@ def is_question(line):
     return question
 
 def punctuation(para):
+    fin = []
+    #words = nltk.tokenize.word_tokenize(para)
+    words = re.split('[ ]',para)
+    cap_sugg = {}
+    for ind in range(len(words)) :
+        word = words[ind]
+        flag = 0
+        ch = ''
+        if (word == ""):
+            continue
+        if (word[-1]=='.' or word[-1]=='?' or word[-1]=='!'):
+            flag = 1
+            ch = word[-1]
+            word=word[:-1]
+        sug = word
+        if (ut.tag([word])[0][1] is None):
+            c=word[0]
+            word1=word[0].upper()+word[1:]
+            cap_sugg[ind]=word1
+            sug = word1
+        sug=sug+ch
+        fin.append(sug)
+    para = TreebankWordDetokenizer().detokenize(fin)
+    print(para)
     arr=re.split('[?.!]',para)
     # print(arr)
     newpara=[]
     for line in arr:
+        line=line.lstrip()
+        line=line.rstrip()
         if(line==''):
             continue
         if(is_question(line)):
