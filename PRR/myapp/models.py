df44523e0f92422de8b71f7a0f7cd4490c23adb1
saypal18 <sayantanpal2000.2018@gmail.com>
2019-11-25 09:21:34 +0530

    djangoforminput

---
+++ PRR/myapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Sentence(models.Model):
+	sentence = models.CharField(max_length = 5000)
+	complete = models.BooleanField(default = False)
+	changed = models.BooleanField(default = False)
+
+	def __str__(self):
+		return self.sentence
+
+class Word(models.Model):
+	sentence = models.ForeignKey(Sentence, on_delete = models.CASCADE)
+	word = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.word
+
+class CorWord(models.Model):
+	word = models.ForeignKey(Word, on_delete = models.CASCADE)
+	corWord = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.corWord
